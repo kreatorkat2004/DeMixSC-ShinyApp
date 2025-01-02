1d6d78462e59e71e8b4324bbfbd8d2d5ab9028a8
Aaron Wu <aw80@rice.edu>
2025-01-01 23:16:37 -0600

    Created new application formatting

--- ui.R
+++ ui.R
@@ -1,60 +1,50 @@
 library(shiny)
 library(plotly)
-library(shinyjs)
 library(DT)
 
 ui <- fluidPage(
-  useShinyjs(),
-  titlePanel("DeMixSC: Deconvolution of Mixed-type Single Cell RNA-seq Data"),
+  titlePanel("DeMixSC Analysis Interface"),
   
-  navbarPage("",
-             # About tab
-             tabPanel("About",
-                      h4("Description"),
-                      p("DeMixSC is a method for deconvoluting mixed-type scRNA-seq data..."),
-                      p("Contact: [Your contact info]")
-             ),
-             
-             # Analysis tab
-             tabPanel("Analysis",
-                      sidebarLayout(
-                        sidebarPanel(
-                          # File upload section
-                          fileInput("countsFile", "Upload Count Matrix:", accept = c(".csv", ".txt")),
-                          fileInput("annotFile", "Upload Cell Type Annotations:", accept = c(".csv", ".txt")),
-                          
-                          # Parameters
-                          numericInput("numClusters", "Number of Clusters:", value = 2, min = 2),
-                          selectInput("normMethod", "Normalization Method:",
-                                      choices = c("LogNormalize", "SCTransform")),
-                          
-                          # Action buttons
-                          actionButton("runAnalysis", "Run Analysis"),
-                          downloadButton("downloadResults", "Download Results")
-                        ),
-                        
-                        mainPanel(
-                          # Results tabs
-                          tabsetPanel(
-                            tabPanel("Visualization",
-                                     plotlyOutput("umap"),
-                                     plotlyOutput("proportions")
-                            ),
-                            tabPanel("Statistics",
-                                     DTOutput("statsTable")
-                            ),
-                            tabPanel("Gene Expression",
-                                     plotlyOutput("geneExpr")
-                            )
-                          )
-                        )
-                      )
-             ),
-             
-             # Examples tab  
-             tabPanel("Examples",
-                      h4("Example Datasets"),
-                      DTOutput("exampleTable")
+  tabsetPanel(
+    # Tab 1: Custom Data Analysis
+    tabPanel("Custom Data Analysis",
+             sidebarLayout(
+               sidebarPanel(
+                 fileInput("countsFile", "Upload Count Matrix:", 
+                           accept = c(".csv", ".txt")),
+                 fileInput("annotFile", "Upload Cell Type Annotations:", 
+                           accept = c(".csv", ".txt")),
+                 numericInput("numClusters", "Number of Clusters:", 
+                              value = 2, min = 2),
+                 selectInput("normMethod", "Normalization Method:",
+                             choices = c("LogNormalize", "SCTransform")),
+                 actionButton("runCustomAnalysis", "Run Analysis")
+               ),
+               mainPanel(
+                 plotlyOutput("customPlot"),
+                 DTOutput("customResults")
+               )
              )
+    ),
+    
+    # Tab 2: Package Data Analysis
+    tabPanel("Package Data Analysis",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("datasetChoice", "Select Dataset:",
+                             choices = c("Retina" = "retina", 
+                                         "HGSC" = "hgsc")),
+                 numericInput("minExpression", "Minimum Expression:",
+                              value = 3),
+                 numericInput("scaleFactor", "Scale Factor:",
+                              value = 1e5),
+                 actionButton("runPackageAnalysis", "Run Analysis")
+               ),
+               mainPanel(
+                 plotlyOutput("packagePlot"),
+                 DTOutput("packageResults")
+               )
+             )
+    )
   )
 )
